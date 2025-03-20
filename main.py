e51d511db22572fcf99b535a44d8a640c7373cf8
Javier272 <147571233+Javier272@users.noreply.github.com>
2025-03-20 16:01:37 +0000

    calculo gradiente

--- main.py
+++ main.py
@@ -14,3 +14,11 @@ imagen_gris=cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)
 #Aplica filtro sobel en X, Y
 xsoble=cv2.Sobel(imagen_gris,cv2.CV_64F,1,0,ksize=5)
 ysobel=cv2.Sobel(imagen_gris,cv2.CV_64F,0,1,ksize=5)
+
+
+#Calcula gradiente
+gradiente=cv2.magnitude(xsoble,ysobel)
+
+
+#Normaliza el gradiente
+normalizado_gradiente=np.uint8(255*(gradiente/np.max(gradiente)))
