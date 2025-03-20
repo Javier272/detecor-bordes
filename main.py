cad7a205463483313333839e3f4a2aebd1da1bc2
Javier272 <147571233+Javier272@users.noreply.github.com>
2025-03-20 16:00:02 +0000

    filtro sobel

--- main.py
+++ main.py
@@ -9,3 +9,8 @@ imagen=cv2.imread("ob9.jpg")
 
 #convierte la imagen a escala de grises
 imagen_gris=cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)
+
+
+#Aplica filtro sobel en X, Y
+xsoble=cv2.Sobel(imagen_gris,cv2.CV_64F,1,0,ksize=5)
+ysobel=cv2.Sobel(imagen_gris,cv2.CV_64F,0,1,ksize=5)
